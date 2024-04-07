1075a36f84be34da3e66b938f0ae2c192b3386fa
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2024-04-07 15:08:47 +0900

    Title: 코드트리 빵, Time: 552ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240407/코드트리 빵/codetree-mon-bread.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+int n,m;  //1 베이스 캠프
+int x,y; 
+vector<vector<int>>basecamp; 
+vector<vector<bool>>base_visited;
+vector<vector<int>>store; //편의점인지  
+vector<vector<bool>>store_visited; //편의점 방문 
+//각 사람들의 위치 
+struct info{
+    int num; 
+    int x;
+    int y; 
+    //편의점에 도착했는지 
+    bool arrive; 
+    //가려는 편의점 위치 
+    int destx;
+    int desty; 
+};
+vector<info>person;
+typedef pair<int,int> ci; 
+int dr[4]={-1,0,0,1};
+int dc[4]={0,-1,1,0}; 
+
+//최단거리 출력 
+int bfs(int nr,int nc,int destr,int destc,int len){
+    
+    //못가는 베이스캠프, 편의점 고려해서 가기 
+    queue<pair<int,ci>>q;
+    q.push({len,{nr,nc}});
+
+    while(!q.empty()){
+      
+      int cur=q.front().second.first;
+      int cuc=q.front().second.second;
+      int cul=q.front().first;
+      q.pop();
+      if(cur==destr && cuc==destc){
+          //작은걸 봐야하나...? 
+          return cul; 
+      }
+      
+      for(int i=0;i<4;i++){
+        int nr=cur+dr[i];
+        int nc=cuc+dc[i]; 
+        if(nr<=0 || nc<=0 || nr>n || nc>n)continue; 
+        if(base_visited[nr][nc] || store_visited[nr][nc])continue;
+        q.push({cul+1,{nr,nc}}); 
+      }
+
+    }    
+}
+
+bool cmp(ci a, ci b){
+    if(a.first==b.first){
+        return a.second<b.second; 
+    }
+    return a.first<b.first;
+}
+
+
+void Move(){
+  //격자 내에 있고, 편의점에 도착하지 않은 사람들 이동 
+  //못지나가는 베이스캠프,편의점 목록
+  vector<ci>no_base;
+  vector<ci>no_store;
+
+  for(int i=0;i<m;i++){
+    if(person[i].arrive)continue; 
+    if(person[i].x<=0 || person[i].y<=0 || person[i].x>n || person[i].y>n)continue; 
+    int cur=person[i].x;
+    int cuc=person[i].y; 
+    //4방향 중 최단거리로 갈수 있는 칸 수 -> 상좌우하 순 
+    int destr=person[i].destx;
+    int destc=person[i].desty; 
+    
+    //거리-방향 
+    vector<ci>tmp;
+    for(int j=0;j<4;j++){
+      int nr=cur+dr[j];
+      int nc=cuc+dc[j]; 
+      if(nr<=0 || nc<=0 || nr>n || nc>n)continue; 
+      if(base_visited[nr][nc] || store_visited[nr][nc])continue;
+      int distance=bfs(nr,nc,destr,destc,1);
+      tmp.push_back({distance,j});  
+    }
+    sort(tmp.begin(),tmp.end(),cmp); 
+    //해당 방향으로 감 
+    //갈수 없는 경우? 
+    if(tmp.size()==0)continue; 
+    int dir=tmp[0].second; 
+   // cout<<i<<' '<<dir<<"방향으로 감\n";
+    //i 사람이 dir 방향에 감 
+    int nx=cur+dr[dir];
+    int ny=cuc+dc[dir];
+    person[i].x=nx;
+    person[i].y=ny;  
+    //베이스캠프나 편의점에 갔는가? -> 더이상 거기 아무도 못지나감, 이거 다 움직이고 
+    //베이스일 경우
+    if(basecamp[nx][ny]==1){
+      //  person[i].x=nx;
+      //  person[i].y=ny; 
+        no_base.push_back({nx,ny});
+    }
+    //편의점일 경우 
+    if(store[nx][ny]==1){
+     //  person[i].x=nx;
+     //  person[i].y=ny; 
+       person[i].arrive=true;
+       no_store.push_back({nx,ny}); 
+    }
+
+  }
+  //이동 끝나고 편의점 도착한 칸 더이상 못가게 하기   
+  for(int i=0;i<no_base.size();i++){
+    base_visited[no_base[i].first][no_base[i].second]=true; 
+  } 
+  for(int i=0;i<no_store.size();i++){
+    store_visited[no_store[i].first][no_store[i].second]=true;
+  } 
+
+}
+
+void goBase(int cnt){
+    if(person[cnt-1].arrive)return; 
+    //cnt번 사람 자기가 가고싶은 편의점과 가장 가까운 베이스켐프가기 
+    int destr=person[cnt-1].destx;
+    int destc=person[cnt-1].desty; 
+
+    queue<pair<int,ci>>q;
+    q.push({0,{destr,destc}});
+    vector<ci>tmp; 
+    //갈수 없는거 잘피해서 가기 
+    int mins=10000000; 
+    while(!q.empty()){
+       int cur=q.front().second.first;
+       int cuc=q.front().second.second; 
+       int cul=q.front().first;
+       q.pop();
+       if(mins<cul)continue; 
+      
+       if(basecamp[cur][cuc]==1){
+         if(mins>cul){
+            mins=min(mins,cul); 
+            tmp.push_back({cur,cuc});
+         } 
+         continue; 
+       }
+       for(int i=0;i<4;i++){
+        int nr=cur+dr[i];
+        int nc=cuc+dc[i]; 
+        if(nr<=0 || nc<=0 || nr>n || nc>n)continue; 
+        if(base_visited[nr][nc] || store_visited[nr][nc])continue;
+        
+        q.push({cul+1,{nr,nc}}); 
+
+       }
+    }
+    //tmp 중에 r,c 가장 작은곳으로 
+    sort(tmp.begin(),tmp.end(),cmp);
+    if(tmp.size()>=1){
+        person[cnt-1].x=tmp[0].first;
+        person[cnt-1].y=tmp[0].second; 
+        base_visited[tmp[0].first][tmp[0].second]=true; 
+    } 
+}
+
+
+void Go(int cnt){
+
+   //격자 내에 있는 사람 움직이기 
+   Move(); 
+   //cnt분이 <=m 이면 cnt번 사람 가고싶은 편의점과 가장 가까운 베이스켐프 이동 
+   if(cnt<=m){
+     goBase(cnt);
+   }
+
+}
+
+bool check(){
+   bool flag=true;
+   for(int i=0;i<m;i++){
+    if(!person[i].arrive)flag=false; 
+   }
+   return flag; 
+}
+void print(){
+    for(int i=0;i<m;i++){
+        cout<<person[i].x<<' '<<person[i].y<<'\n';
+    }
+    cout<<'\n';
+}
+
+int main() {
+    
+    cin>>n>>m;
+    basecamp.assign(n+1,vector<int>(n+1,0));
+    base_visited.assign(n+1,vector<bool>(n+1,0));
+    store.assign(n+1,vector<int>(n+1,0));
+    store_visited.assign(n+1,vector<bool>(n+1,0)); 
+
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n;j++){
+         cin>>basecamp[i][j]; 
+      }
+    }
+
+    for(int i=1;i<=m;i++){
+        cin>>x>>y; 
+        person.push_back({i,-1,-1,0,x,y}); 
+        store[x][y]=1; 
+    }
+    int cnt=1; 
+    while(1){
+       if(check())break; 
+       Go(cnt);
+      // print(); 
+       cnt++;
+
+    }
+    cout<<cnt-1; 
+
+    return 0;
+}
