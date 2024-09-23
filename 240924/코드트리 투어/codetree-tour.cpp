aba59f818e675cdd3618a276fa496caa5b726de8
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2024-09-24 07:43:22 +0900

    Title: 코드트리 투어, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240924/코드트리 투어/codetree-tour.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+
+    int q;//명령어 수 
+    cin>>q;
+    for(int i=0;i<q;i++){
+       int num;
+       cin>>num;
+       if(num==100){  //코드트리 랜드 건설
+          int n,m,v,u,w; 
+          cin>>n>>m; 
+ 
+
+
+
+
+       }
+       else if(num==200){ //여행 상품 생성
+
+       }
+       else if(num==300){ //여행 상품 취소 
+
+       }
+       else if(num==400){//최적의 여행 상품 판매 
+
+       }
+       else{//500  //출발지 변경 
+
+       }
+
+
+
+
+
+    }
+
+
+
+
+    return 0;
+}
