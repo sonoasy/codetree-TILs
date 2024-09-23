9fb9221160703f7e9130bac065910d0f53545cf6
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2024-09-23 21:30:20 +0900

    Title: 놀이기구 탑승, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/놀이기구 탑승/go-on-the-rides.cpp
+++ 240923/놀이기구 탑승/go-on-the-rides.cpp
@@ -1,104 +1,125 @@
 #include <iostream>
-#include<vector> 
 #include<map>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-int n; 
-map<int,vector<int>>infos; //좋아하는 친구 
+int dr[4]={0,0,1,-1};
+int dc[4]={1,-1,0,0};
+ //격자에서 4방향으로 자기가 좋아하는 친구 수 가장 많은곳 찾기 
+ //인접한 칸 비어있는 수 많은 위치 (격자 밖x)
+ //행 작 -> 열 작 
+struct friends{
+    int good_num;
+    int empty_cnt;
+    int r;
+    int c;
+   // bool operator<(friends right)const {
+        //min heap 
+    //    if (good_num>right.good_num)return true; 
+    //    if (empty_cnt>right.empty_cnt)return true;
+    //    if (r<right.r)return true;
+    //    if (c<right.c)return true;
+  //  }
+    //잘 모르겟...     
+};
+bool cmp(friends a,friends b){
+    
+    if(a.good_num==b.good_num){
+       if(a.empty_cnt==b.empty_cnt){
+         if(a.r==b.r){
+            return a.c<b.c;
+         }
+         return a.r<b.r;
+       }
+       return a.empty_cnt>b.empty_cnt;
+    }
+    return a.good_num>b.good_num;
+}
+
+//좋아하는 친구 목록 
+map<int,vector<int>>infos;
+int n; //격자 크기 
 //놀이기구 
 vector<vector<int>>arr;
-//줄 리스트
-vector<int>lists; 
-
-int dr[4]={0,0,1,-1};
-int dc[4]={1,-1,0,0}; 
-
-struct friends{
-  //좋아하는 친구 수 
-  int num;
-  //빈칸 갯수 
-  int empty_cnt; 
-  //행 
-  int r;
-  //열  
-  int c; 
-   
-  bool operator<(friends right)const{
-    //친구 수 많은 -> 빈칸 갯수 많은 -> 행 작 -> 열 작
-    if(num>right.num)return true;
-    if(empty_cnt>right.empty_cnt)return true;
-    if(r<right.r)return true;
-    if(c<right.c)return true;
-  }
-}; 
-
+vector<int>lines;
 void go(){
-   //각각의 대기명단에서 
-   for(int i=0;i<lists.size();i++){
-      //확인하려는 번호 
-      int num=lists[i];
-      vector<friends>tmp; 
-      //격자내 모든 공간 
-      for(int j=0;j<n;j++){
-        for(int k=0;k<n;k++){
-          if(arr[j][k]!=0)continue; //번호가 있음 
-          int g=0; int c=0;   
 
-          for(int s=0;s<4;s++){
-            //(j,k) 격자에 대하여 
-            int nr=j+dr[s];
-            int nc=k+dc[s]; 
+   for(int l=0;l<lines.size();l++){  
+    int num=lines[l]; //지금 놓으려는 번호 
 
-            if(nr<0 || nc<0 || nr>=n || nc>=n)continue; 
-            //빈칸 갯수 
-            if(arr[nr][nc]==0)c++;
-            else{ //좋아하는 친구 수 
-                for(int kk=0;kk<infos[num].size();kk++){
-                    int nn=infos[num][kk];
-                    if(arr[nr][nc]==nn)g++;  //0이 아닌데 좋아하는 친구 목록에 있으면 +1  
+    vector<friends>tmp; 
+   
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]>0)continue;
+           //좋아하는 친구 수
+           //비어있는 수 
+            int g=0;
+            int e=0;
+           for(int k=0;k<4;k++){
+             int nr=i+dr[k];
+             int nc=j+dc[k];
+             if(nr<0 || nc<0 || nr>=n || nc>=n)continue; 
+             if(arr[nr][nc]>0){
+                //누군가 있다 
+                for(int s=0;s<infos[num].size();s++){
+                    if(arr[nr][nc]==infos[num][s])g++;
                 }
-            }    
-          }
-          
-          tmp.push_back({g,c,j,k}); 
-
+             }
+             else{ //빈자리 세기 
+                e++;
+             } 
+           }
+           tmp.push_back({g,e,i,j});
+           
         }
-      }
+    }
+    sort(tmp.begin(),tmp.end(),cmp);
 
-      cout<<num<<"일떄\n";
-      for(int a=0;a<tmp.size();a++){
-         cout<<tmp[a].r<<' '<<tmp[a].c<<' '<<tmp
-       }
-
-      //num이 가야 할 위치 
+      //가장 첫번째 자리로 감 
       arr[tmp[0].r][tmp[0].c]=num;
-      cout<<num<<" "<<tmp[0].r<<','<<tmp[0].c<<"으로감\n"; 
-
    }
-
-
-
-
 }
 
 
 
-
 int main() {
-     
-     cin>>n; 
-     arr.assign(n,vector<int>(n,0));
-     int num,a,b,c,d;
-     for(int i=0;i<n*n;i++){
-      cin>>num>>a>>b>>c>>d; 
-      infos[num].push_back(a);
-      infos[num].push_back(b);
-      infos[num].push_back(c);
-      infos[num].push_back(d);
-      lists.push_back(num);
-     }
-     go(); 
 
+    
+    cin>>n;
+    arr.assign(n,vector<int>(n,0));
+    int num;
+    int a,b,c,d; 
+    for(int i=0;i<n*n;i++){
+       cin>>num>>a>>b>>c>>d;
+       infos[num].push_back(a);
+       infos[num].push_back(b);
+       infos[num].push_back(c);
+       infos[num].push_back(d);
+       lines.push_back(num); 
+    }
+    go();
+    int ans=0; 
+    int score[5]={0,1,10,100,1000};
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           int cnt=0; 
+           int nums=arr[i][j]; 
+           //
+           for(int k=0;k<4;k++){
+              int nr=i+dr[k];
+              int nc=j+dc[k];
+              if(nr<0 || nc<0 || nr>=n || nc>=n)continue; 
+              for(int s=0;s<infos[nums].size();s++){
+                 if(arr[nr][nc]==infos[nums][s])cnt++; 
+              }
+           }
+           ans+=score[cnt];
+
+        }
+    }
+    cout<<ans;
     return 0;
 }
