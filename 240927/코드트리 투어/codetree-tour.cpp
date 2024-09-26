0b30dc4e841acc378eca88c3c9b3ec180fe47ea2
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2024-09-27 06:38:17 +0900

    Title: 코드트리 투어, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240927/코드트리 투어/codetree-tour.cpp
+++ 240927/코드트리 투어/codetree-tour.cpp
@@ -97,14 +97,14 @@ int main() {
           int tcost; 
           int target;
           int mid=1e9;
-          cout<<"이윤 목록\n";  
+         // cout<<"이윤 목록\n";  
           //최대 n개만 돌면됨 
           for(auto it=info.begin();it!=info.end();it++){
              //비어있으면 넘어가기 
              if(it->second.size()==0)continue;
              tid=it->second.begin()->first;
              tcost=it->second.begin()->second;
-            cout<<"id: "<<tid<<"이윤 "<<tcost<<'\n';
+           // cout<<"id: "<<tid<<"이윤 "<<tcost<<'\n';
              if(tcost>=maxs){ //set이 정렬이 안되는거야??????
                //if(mid>tid){ 
                   //값이 같을땐 id가 작은 경우에면 
